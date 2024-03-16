8f0228deacdc83cde4253a4001bde7b518fd505b
Mark Villudo <markanthony.villudo@gmail.com>
2024-03-16 09:40:37 +0800

    add validation

--- app/Http/Controllers/Api/Mail/PoftfolioEmailController.php
+++ app/Http/Controllers/Api/Mail/PoftfolioEmailController.php
@@ -11,17 +11,25 @@ use App\Mail\PortfolioMail;
 class PoftfolioEmailController extends Controller
 {
     //
-    public function sendMail()
+    public function sendMail(Request $request)
     {
+       // Validate the incoming request data
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'message' => 'required|string|max:1000',
+        ]);
+
+        // If validation passes, proceed to send the email
         $content = [
             'subject' => 'This is the mail subject',
-            'body' => 'This is the email body of how to send email from laravel 10 with mailtrap.',
-            'name' => request('name'),
-            'email' => request('email'),
-            'message' => request('message'),
+            'body' => 'This is the email body of how to send email from Laravel 10 with Mailtrap.',
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'message' => $validatedData['message'],
         ];
 
-        Mail::to('villludomark@gmail.com')->send(new PortfolioMail($content));
+        Mail::to($validatedData['email'])->send(new PortfolioMail($content));
 
         return "Email has been sent.";
     }
